a626ade44ec981a2a45bb1d6e1a88467641d5496
Abhyuday Chauhan <abhyuday2003chauhan@gmail.com>
2024-02-23 18:29:28 +0530

    Update

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ImagePreprocessing",
     version="1.0",
+    author="Abhyuday Chauhan",
     description="Personal project, will make changes as per use case",
     packages=find_packages(),
     install_requires=["opencv-python", "numpy"]
