0c2d126824aa7b3fb50cbb0112517a4959ec7045
Abhyuday Chauhan <abhyuday2003chauhan@gmail.com>
2024-03-12 09:45:16 +0530

    Update

--- Operations/operation.py
+++ Operations/operation.py
@@ -10,78 +10,129 @@ class Resize:
         self.fy = fy
         self.interpolation = interpolation
 
-    def run(self, images: np.ndarray, bounding_boxes: np.ndarray) -> np.ndarray[np.ndarray]:
-
-        return images, bounding_boxes
+    def run(self, image: np.ndarray, bounding_boxes: np.ndarray) -> np.ndarray:
+        img_shape = np.shape(image)[:-1]
+        return image, bounding_boxes
 
 
 class Color:
 
-    def __init__(self, code: int, dst: int, dstCn: int):
+    def __init__(self, code: int):
         self.code = code
-        self.dst = dst
-        self.dstCn = dstCn
 
-    def run(self, images: np.ndarray) -> np.ndarray:
-        images = cv2.cvtColor(code=self.code, dst=self.dst)
-        return images
+    def run(self, image: np.ndarray) -> np.ndarray:
+        dstCn = np.shape(image)[2]
+        image = cv2.cvtColor(image, code=self.code, dstCn=dstCn)
+        return image
 
 
 class Normalize:
-    def __init__(self):
-        pass
+    def __init__(self, alpha: float, beta: float, norm_type: int = cv2.NORM_MINMAX):
+        self.norm_type = norm_type
+        self.alpha = alpha
+        self.beta = beta
 
-    def run(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.normalize
-        return images
+    def run(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.normalize(image,
+                              alpha=self.alpha,
+                              beta=self.beta,
+                              norm_type=self.norm_type
+                              )
+        return image
 
 
 class Blur:
 
-    def __init__(self, blur_type: int, borderType: int = cv2.BORDER_DEFAULT):
-        self.blurType = blur_type
-        self.blur_functions = [
-                self.box_filter,
-                self.gaussian_blur,
-                self.simple_blur,
-                self.median_blur,
-                self.bilateral_blur
-            ]
+    def __init__(self,
+                 blur_type: int,
+                 ksize: np.ndarray | None,
+                 ddepth: int = 3,
+                 sigmaY: float = 0,
+                 sigmaX: float = 1,
+                 borderType: int = cv2.BORDER_DEFAULT,
+                 normalize: bool = False,
+                 sigmaColor: float = 75,
+                 sigmaSpace: float = 75,
+                 d: int = 15
+                 ):
 
-    def run(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
+        self.blurType = blur_type
+        self.ksize = ksize
+        self.ddepth = ddepth
+        self.sigmaX = sigmaX
+        self.sigmaY = sigmaX if sigmaY == 0 else sigmaY
+        self.borderType = borderType
+        self.normalize = normalize
+        self.sigmaColor = sigmaColor
+        self.sigmaSpace = sigmaSpace
+        self.d = d
+
+    def run(self, image: np.ndarray) -> np.ndarray:
         if self.blurType == 0:
-            images = self.box_filter(images=images)
+            images = self.box_filter(image=image)
         elif self.blurType == 1:
-            images = self.gaussian_blur(images=images)
+            images = self.gaussian_blur(image=image)
         elif self.blurType == 2:
-            images = self.simple_blur(images=images)
+            images = self.simple_blur(image=image)
         elif self.blurType == 3:
-            images = self.median_blur(images=images)
+            images = self.median_blur(image=image)
         elif self.blurType == 4:
-            images = self.bilateral_blur(images=images)
+            images = self.bilateral_blur(image=image)
+        elif self.blurType == 5:
+            images = self.SobelFilter(image=image)
         else:
             raise ValueError
         return images
 
-    def gaussian_blur(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.GaussianBlur
-        return images
+    def gaussian_blur(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.GaussianBlur(image,
+                                 ksize=self.ksize,
+                                 sigmaX=self.sigmaX,
+                                 sigmaY=self.sigmaY,
+                                 borderType=self.borderType
+                                 )
+        return image
 
-    def box_filter(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.boxFilter
-        return images
+    def box_filter(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.boxFilter(image,
+                              ddepth=self.ddepth,
+                              ksize=self.ksize,
+                              normalize=self.normalize,
+                              borderType=self.borderType
+                              )
+        return image
 
-    def simple_blur(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.blur
-        return images
+    def simple_blur(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.blur(image,
+                         ksize=self.ksize,
+                         borderType=self.borderType
+                         )
+        return image
 
-    def median_blur(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.medianBlur
-        return images
+    def median_blur(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.medianBlur(image,
+                               ksize=self.ksize
+                               )
+        return image
 
-    def bilateral_blur(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.bilateralFilter
-        return images
+    def bilateral_blur(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.bilateralFilter(image,
+                                    d=self.d,
+                                    sigmaColor=self.sigmaColor,
+                                    sigmaSpace=self.sigmaSpace
+                                    )
+        return image
+
+    def SobelFilter(self, image: np.ndarray) -> np.ndarray:
+        SOBELKERNAL = np.array([
+            [-1, 0, 1],
+            [-2, 0, 2],
+            [-1, 0, 1]
+        ])
+        img_x = cv2.filter2D(image, cv2.CV_64F, kernel=SOBELKERNAL)
+        img_y = cv2.filter2D(image, cv2.CV_64F, kernel=SOBELKERNAL.transpose)
+        img = cv2.normalize(np.sqrt(img_x**2+img_y**2), None, 0, 255, cv2.NORM_MINMAX)
+        return img
 
 
 class Filter2d:
@@ -92,6 +143,23 @@ class Filter2d:
         self.delta = delta
         self.borderType = borderType
 
-    def run(self, images: np.ndarray) -> np.ndarray[np.ndarray]:
-        func = cv2.filter2D
-        return images
+    def run(self, image: np.ndarray) -> np.ndarray:
+        image = cv2.filter2D(image,
+                             ddepth=self.ddpth,
+                             delta=self.delta,
+                             borderType=self.borderType,
+                             kernel=self.kernal
+                             )
+        return image
+
+
+class CustomFunc:
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self, image: np.ndarray) -> np.ndarray:
+        image = self.func(image, *self.args, **self.kwargs)
+        return image
+
--- Operations/types.py
+++ Operations/types.py
@@ -3,3 +3,4 @@ GAUSSIAN: int = 1
 SIMPLE_BLUR: int = 2
 MEDIAN_BLUR: int = 3
 BILATERAL_BLUR: int = 4
+SOBELFILTER: int = 5
