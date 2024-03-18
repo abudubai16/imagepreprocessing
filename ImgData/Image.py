1cbe20a1feb162d02025422d032df2abe56609da
Abhyuday Chauhan <abhyuday2003chauhan@gmail.com>
2024-03-18 19:00:36 +0530

    Update

--- ImgData/Image.py
+++ ImgData/Image.py
@@ -37,6 +37,7 @@ class Image:
 
 
 class Sequential:
+    dir: str
     def __init__(self, operations: list):
         self.operations = operations
 
@@ -49,8 +50,9 @@ class Sequential:
         if chunksize == 1:
             chunksize = int(max(len(data.current_paths)/100, 1))
 
-        os.chdir(data.directory)
-        processed_path = f"{data.directory}/processed_images"
+        self.dir = data.directory
+        os.chdir(self.dir)
+        processed_path = f"{self.dir}/processed_images"
         if not os.path.exists(processed_path):
             os.mkdir(processed_path)
 
@@ -63,15 +65,15 @@ class Sequential:
     def run_sequential(self, img_path: str) -> bool:
         try:
             bb = None
-            cwd = os.getcwd()
-            img = cv2.imread(f"{cwd}/{img_path}")
+            img = cv2.imread(f"{self.dir}/{img_path}")
+            print(f"{self.dir}/{img_path}")
             for img_operation in self.operations:
 
                 if img_operation == Operations.Resize:
                     img, bb = img_operation.run(img, bb)
                 else:
                     img = img_operation.run(img)
-            cv2.imwrite(f"{cwd}/processed_images/{img_path}", img)
+            cv2.imwrite(f"{self.dir}/processed_images/{img_path}", img)
 
             return True
         except TypeError:
